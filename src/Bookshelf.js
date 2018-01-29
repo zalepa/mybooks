de102d00f7218601d32df3d8a005d5154656487d
George Zalepa <george.zalepa@gmail.com>
2018-01-29 17:37:00 -0500

    Reorganize static layout into components

---
+++ src/Bookshelf.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Book from './Book';
+class Bookshelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Bookshelf;
