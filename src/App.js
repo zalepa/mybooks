a171b6f809a36983fcf3dc5196d903ac8d5ee953
George Zalepa <george.zalepa@gmail.com>
2018-01-30 17:40:17 -0500

    Code cleanup of App

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ class BooksApp extends React.Component {
   placeBooksToShelfState = (books) => {
     this.setState({
       shelves: {
-        currentlyReading: books.filter(book => book.shelf === 'currentlyReading'),
+        currentlyReading: books.filter(book => (
+          book.shelf === 'currentlyReading'
+        )),
         wantToRead: books.filter(book => book.shelf === 'wantToRead'),
         read: books.filter(book => book.shelf === 'read')
       }
@@ -42,21 +44,28 @@ class BooksApp extends React.Component {
     return (
       <BrowserRouter>
           <div className="app">
+
             <Route exact path="/search" component={Search}/>
-            <Route exact path="/" render={() => (
-              <div className="list-books">
+
+          <Route exact path="/" render={() => (
+
+                <div className="list-books">
                 <div className="list-books-title"><h1>MyReads</h1></div>
                 <div className="list-books-content">
                   <div>
-                    <Bookshelf title="Currently Reading" books={this.state.shelves.currentlyReading} />
-                    <Bookshelf title="Want to Read" books={this.state.shelves.wantToRead} />
-                    <Bookshelf title="Read" books={this.state.shelves.read} />
+                    <Bookshelf title="Currently Reading"
+                               books={this.state.shelves.currentlyReading} />
+                    <Bookshelf title="Want to Read"
+                               books={this.state.shelves.wantToRead} />
+                    <Bookshelf title="Read"
+                               books={this.state.shelves.read} />
                   </div>
                 </div>
                 <div className="open-search">
                   <Link to="/search">Add a book</Link>
                 </div>
               </div>
+
             )} />
           </div>
       </BrowserRouter>
