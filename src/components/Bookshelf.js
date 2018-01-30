18e13b9a28fcec67bf6dcf60c10414e54afc7bc7
George Zalepa <george.zalepa@gmail.com>
2018-01-30 17:43:11 -0500

    Cleanup Bookshelf; convert to stateless component

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
-class Bookshelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.map((book, index) => (
-              <li key={index}>
-                <Book book={book} />
-              </li>
-            ))}
-          </ol>
-        </div>
+
+function Bookshelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">
+        {props.title}
+      </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+
+          {props.books.map(book => (
+            <li key={book.id}>
+              <Book book={book} />
+            </li>
+          ))}
+
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Bookshelf;
